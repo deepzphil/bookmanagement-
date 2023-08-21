1d97968362b319e5277dc7e3e37cf2aa93cd66d3
Deepu Philip <deepujessphilip@gmail.com>
2023-08-21 19:06:21 +0100

    Update Book.java
    commenting on the validation process

--- bookmanagement/src/main/java/com/bookmanagement/bookmanagement/model/Book.java
+++ bookmanagement/src/main/java/com/bookmanagement/bookmanagement/model/Book.java
@@ -45,13 +45,13 @@ public class Book {
 	public void setPublicationYear(int publicationYear) {
 		this.publicationYear = publicationYear;
 	}
-
+//to validate that title is not null
 	@Nonnull
     private String title;
-
+//to validate that author is null
     @Nonnull
     private String author;
-
+//to validate publicationYear is not null
     @Nonnull
     private int publicationYear;
 	
